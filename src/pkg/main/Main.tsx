5793b68c06f3a24891aeb3445616cb82fbe586a7
Quang Le <phuc.quang102@gmail.com>
2021-11-04 23:04:47 +0700

    Relayout main form

--- src/pkg/main/Main.tsx
+++ src/pkg/main/Main.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import {Button, FloatingLabel, Form} from "react-bootstrap";
+import React, {FunctionComponent} from "react";
+import {Accordion, Button, Card, FloatingLabel, Form, useAccordionButton} from "react-bootstrap";
 import BaseTransformer from "../transformers/BaseTransformer";
 import NoOps from "../transformers/NoOps";
 
@@ -10,6 +10,42 @@ interface State {
   output?: string;
   transformers: BaseTransformer[];
 }
+
+interface CustomToggleProps {
+  eventKey: string
+}
+
+const CustomToggle: FunctionComponent<CustomToggleProps> = ({eventKey, children}) => {
+  const decoratedOnClick = useAccordionButton(eventKey, () =>
+      console.log('totally custom!'),
+  );
+
+  return (
+      <button
+          type="button"
+          style={{backgroundColor: 'pink'}}
+          onClick={decoratedOnClick}
+      >
+        {children}
+      </button>
+  );
+}
+
+// function CustomToggle(children: React.ReactNode, eventKey: string) {
+//   const decoratedOnClick = useAccordionButton(eventKey, () =>
+//       console.log('totally custom!'),
+//   );
+//
+//   return (
+//       <button
+//           type="button"
+//           style={{ backgroundColor: 'pink' }}
+//           onClick={decoratedOnClick}
+//       >
+//         {children}
+//       </button>
+//   );
+// }
 
 class Main extends React.Component<Props, State> {
   constructor(props: Props, context: any) {
@@ -46,6 +82,25 @@ class Main extends React.Component<Props, State> {
             onChange={(event) => this.handleInputChange(event)}
         />
       </FloatingLabel>
+
+      <Accordion defaultActiveKey="0">
+        <Card>
+          <Card.Header>
+            <CustomToggle eventKey="0">Click me!</CustomToggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>Hello! I'm the body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Card.Header>
+            <CustomToggle eventKey="1">Click me!</CustomToggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>Hello! I'm another body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
 
       {this.renderNodes()}
 
