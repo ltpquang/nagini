4767089054bac8aac5b1eed3890ddbec6b6fffb5
Quang Le <phuc.quang102@gmail.com>
2021-11-18 20:59:04 +0700

    Add json viewer

--- src/pkg/ui/Main.tsx
+++ src/pkg/ui/Main.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import {Col, FloatingLabel, Form, Row} from "react-bootstrap";
 import TransformEngine from "../base/TransformEngine";
 import TransformEngineComponent from "./TransformEngineComponent";
+import ReactJson from "react-json-view";
 
 export const Main = () => {
   const [input, setInput] = useState<string>("");
@@ -11,6 +12,25 @@ export const Main = () => {
   useEffect(() => {
     setOutput(engine.transformData(input))
   }, [engine, input])
+
+  const renderOutput = (output: string) => {
+    let obj = {}
+    try {
+      obj = JSON.parse(input);
+      return <ReactJson src={obj}/>
+    } catch (e) {
+      return (
+          <FloatingLabel controlId="outputTextArea" label="Output">
+            <Form.Control
+                as="textarea"
+                placeholder="Paste"
+                style={{height: '100px'}}
+                value={output}
+                readOnly={true} />
+          </FloatingLabel>
+      )
+    }
+  }
 
   return (
       <div className="Main">
@@ -33,15 +53,7 @@ export const Main = () => {
             </Row>
           </Col>
           <Col md={{span: 7}}>
-            <FloatingLabel controlId="outputTextArea" label="Output">
-              <Form.Control
-                  as="textarea"
-                  placeholder="Paste"
-                  style={{height: '100px'}}
-                  value={output}
-                  readOnly={true}
-              />
-            </FloatingLabel>
+            {renderOutput(output)}
           </Col>
         </Row>
       </div>
