2fff8f5964a890b0f3deca600cec82a48927847f
Quang Le <phuc.quang102@gmail.com>
2021-11-21 01:26:53 +0700

    Json beautify instruction

--- src/pkg/ui/TransformerOptionsJsonBeautify.tsx
+++ src/pkg/ui/TransformerOptionsJsonBeautify.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import JsonBeautify from "../transformers/JsonBeautify";
 import Toggle from "react-toggle";
+import {Col, OverlayTrigger, Row, Tooltip} from "react-bootstrap";
+import {InfoCircle} from "react-bootstrap-icons";
 
 interface Props {
   transformer: JsonBeautify
@@ -15,14 +17,29 @@ export const TransformerOptionsJsonBeautify = ({transformer, onChange}: Props) =
   }
 
   return (
-      <div>
-        <label>
-          <Toggle
-              defaultChecked={false}
-              icons={false}
-              onChange={(event: { currentTarget: { checked: any; }; }) => setPermissive({...transformer, permissive: event.currentTarget.checked})} />
-          <span>Permissive</span>
-        </label>
-      </div>
+      <Row>
+        <Col>
+          <div>
+            Permissive
+            <OverlayTrigger
+                placement="right"
+                overlay={<Tooltip>Try to parse malformed JSON permissively, may NOT keep the JSON
+                  original's structure</Tooltip>}>
+              <InfoCircle className="text-primary ms-2"/>
+            </OverlayTrigger>
+          </div>
+        </Col>
+        <Col>
+          <div className="d-flex justify-content-end">
+            <Toggle
+                defaultChecked={transformer.permissive}
+                icons={false}
+                onChange={(event: { currentTarget: { checked: any; }; }) => setPermissive({
+                  ...transformer,
+                  permissive: event.currentTarget.checked
+                })}/>
+          </div>
+        </Col>
+      </Row>
   )
 }
